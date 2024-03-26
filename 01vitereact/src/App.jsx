3773de35b33176b588d84448092735229197e416
Asta kk <johnkar423@gmail.com>
2024-03-26 11:41:28 +0530

    first vitereact project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+
+
+  return (
+    <h1>chai aur react | Mr Astakar </h1>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,12 @@
+// here we are importing the file where we write the code tha is app.jsx we are doing it in react usit vite
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
